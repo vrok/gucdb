93017ee08c927a8e1f86b3a9d09da4156784c5fc
Marcin Wrochniak <wrochniak@gmail.com>
2011-12-11 17:07:56 +0100

    Unnecessary comments removed

--- test/TrieLeaf_test.cpp
+++ test/TrieLeaf_test.cpp
@@ -12,34 +12,20 @@
 
 namespace {
 
-// The fixture for testing class Foo.
 class TrieLeafTest : public ::testing::Test {
- protected:
-  // You can remove any or all of the following functions if its body
-  // is empty.
+protected:
 
   TrieLeafTest() {
-    // You can do set-up work for each test here.
   }
 
   virtual ~TrieLeafTest() {
-    // You can do clean-up work that doesn't throw exceptions here.
   }
 
-  // If the constructor and destructor are not enough for setting up
-  // and cleaning up each test, you can define the following methods:
-
   virtual void SetUp() {
-    // Code here will be called immediately after the constructor (right
-    // before each test).
   }
 
   virtual void TearDown() {
-    // Code here will be called immediately after each test (right
-    // before the destructor).
   }
-
-  // Objects declared here can be used by all tests in the test case for Foo.
 };
 
 TEST_F(TrieLeafTest, TestGetAddGet)
@@ -59,21 +45,6 @@ TEST_F(TrieLeafTest, TestGetAddGet)
 
     ASSERT_EQ(2357, leaf.get(key, 0));
 }
-
-#if 0
-// Tests that the Foo::Bar() method does Abc.
-TEST_F(FooTest, MethodBarDoesAbc) {
-  const string input_filepath = "this/package/testdata/myinputfile.dat";
-  const string output_filepath = "this/package/testdata/myoutputfile.dat";
-  Foo f;
-  EXPECT_EQ(0, f.Bar(input_filepath, output_filepath));
-}
-
-// Tests that Foo does Xyz.
-TEST_F(FooTest, DoesXyz) {
-  // Exercises the Xyz feature of Foo.
-}
-#endif
 
 }  // namespace
 
