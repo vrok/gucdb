a8bea95b64846f9507e4940cd322e9ef91135d35
Marcin Wrochniak <wrochniak@gmail.com>
2012-01-01 18:00:06 +0100

    Improvement in test generator: can save output to specified files

--- utils/test_gen.py
+++ utils/test_gen.py
@@ -1,5 +1,7 @@
+#!/usr/bin/python
 
 import random
+import sys
 
 MAX_LEN = 3
 TEST_LEN = 20
@@ -32,14 +34,28 @@ for i in xrange(TEST_LEN):
         input_lines.append('read %s' % key)
         output_lines.append(str(db[key]))
 
-print
-print 'INPUT:'
-print '-------'
-print '\n'.join(input_lines)
 
-print
-print 'OUTPUT:'
-print '-------'
-print '\n'.join(output_lines)
+if len(sys.argv) > 1:
+    if len(sys.argv) != 3:
+        print 'You must provide 0 or 2 extra arguments'
+        sys.exit(-1)
+    
+    with open(sys.argv[1], 'w') as generated_input_file:
+        for line in input_lines:
+            generated_input_file.write(line + '\n')
+
+    with open(sys.argv[2], 'w') as generated_output_file:
+        for line in output_lines:
+            generated_output_file.write(line + '\n')
+else:
+    print
+    print 'INPUT:'
+    print '-------'
+    print '\n'.join(input_lines)
+
+    print
+    print 'OUTPUT:'
+    print '-------'
+    print '\n'.join(output_lines)
 
 
