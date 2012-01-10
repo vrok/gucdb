c819676e13fe16b8d7a649d0a9abe31ea2440f57
Marcin Wrochniak <wrochniak@gmail.com>
2012-01-10 22:22:04 +0100

    Created Slabs module

---
+++ modules/Slabs.cpp
@@ -0,0 +1,17 @@
+/*
+ * Slabs.cpp
+ *
+ *  Created on: 10-01-2012
+ *      Author: m
+ */
+
+#include "Slabs.h"
+
+namespace Db {
+
+Slabs::Slabs() {
+    // TODO Auto-generated constructor stub
+
+}
+
+} /* namespace Db */
---
+++ modules/Slabs.h
@@ -0,0 +1,19 @@
+/*
+ * Slabs.h
+ *
+ *  Created on: 10-01-2012
+ *      Author: m
+ */
+
+#ifndef SLABS_H_
+#define SLABS_H_
+
+namespace Db {
+
+class Slabs {
+public:
+    Slabs();
+};
+
+} /* namespace Db */
+#endif /* SLABS_H_ */
