e41e177a27c4fbe5f91836e335c15fb4d2a2237a
Marcin Wrochniak <wrochniak@gmail.com>
2012-05-16 22:51:30 +0200

    Minor modifications in main.cpp sample interface

--- main.cpp
+++ main.cpp
@@ -7,53 +7,69 @@ using namespace std;
 #include "modules/Database.h"
 #include "modules/DatabaseKey.h"
 
-int main(int argc, char *argv[])
+
+
+class Adapter
 {
-    string cmd, key, value;
-    //unsigned long long value;
-    Db::Database dbInstance("/tmp/");
-    unsigned long long counter = 0;
+private:
+    Db::Database dbInstance;
     Db::DatabaseKey dbKey;
-    //Db::Value dbValue;
+
+public:
+
+    Adapter() : dbInstance("/tmp/") {
+    }
+
+    string read(const string & key) {
+        dbKey = key;
+        Db::Value val = dbInstance.read(dbKey);
+        return string(val.data, val.dataSize);
+    }
+
+    void write(const string & key, const string & value) {
+        dbKey = key;
+        Db::Value dbValue(value);
+        dbInstance.write(dbKey, dbValue);
+    }
+
+    void remove(const string & key) {
+        dbKey = key;
+        dbInstance.remove(dbKey);
+    }
+};
+
+template<typename A>
+void dbLoop() {
+    A a;
+    string cmd, key, value;
 
     while (1) {
-        if (counter++ == 2286) {
-            //cout << counter << endl;
-        }
         cout << "> ";
         cout.flush();
         cin >> cmd;
         if (cmd == "read") {
             cin >> key;
-            //cin >> value;
-            dbKey = key;
-            cout << dbInstance.read(dbKey) << endl;
-            //dbInstance.read(key.c_str());
+            cout << a.read(key) << endl;
         } else
         if (cmd == "write") {
             cin >> key >> value;
-
-            dbKey = key;
-            Db::Value dbValue(value);
-
-            cout << dbInstance.write(dbKey, dbValue) << endl;
-            //dbInstance.write(key.c_str(), value);
+            a.write(key, value);
         } else
         if (cmd == "remove") {
             cin >> key;
-            dbKey = key;
-            cout << dbInstance.remove(dbKey) << endl;
-            //dbInstance.remove(key.c_str());
-        } else
-        if (cmd == "dump") {
-            dbInstance.dump();
+            a.remove(key);
         } else
         if (cmd == "exit") {
-            return 0;
+            return;
         } else {
             cerr << "Unknown command" << endl;
         }
     }
-	return 0;
+}
+
+
+int main(int argc, char *argv[])
+{
+    dbLoop<Adapter>();
 }
 
--- modules/DatabaseKey.h
+++ modules/DatabaseKey.h
@@ -20,10 +20,22 @@ public:
     int length;
 
     DatabaseKey();
+
+    DatabaseKey(unsigned char *data, int length) : length(length) {
+        assert(length <= sizeof(data));
+        memcpy(this->data, data, length);
+    }
+
     ~DatabaseKey();
 
+    void update(unsigned char *data, int length) {
+        assert(length <= sizeof(this->data));
+        this->length = length;
+        memcpy(this->data, data, length);
+    }
+
     DatabaseKey * operator=(const std::string & str) {
-        assert(str.length() <= sizeof(data));
+        assert(str.length() <= sizeof(this->data));
 
         memcpy(data, str.c_str(), str.length());
         length = str.length();
