2244919d79219bb7a9999e19530233b17256f509
Marcin Wrochniak <wrochniak@gmail.com>
2013-12-08 21:59:36 +0100

    Add copyright headers (GPLv3) before publishing

--- modules/Trie.cpp
+++ modules/Trie.cpp
@@ -1,4 +1,21 @@
 /*
+    Copyright 2013 Marcin Wrochniak
+
+    This file is part of Guc DB.
+
+    Guc DB is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, version 3 of the License.
+
+    Guc DB is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+*/
+/*
  * Trie.cpp
  *
  *  Created on: 05-12-2011
